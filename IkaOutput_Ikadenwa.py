17cd054898e18d5e6b989aa61b7d9be33ea1d07d
Takeshi HASEGAWA <hasegaw@gmail.com>
2015-09-24 13:02:25 +0900

    IkaOutput_Ikadenwa: Updated.
    Now IkaLog has all of required features expected for Ikadenwa support.
    Below describes  how it works:
    
    dhcp44-82:IkaLog hasegaw$ python3 IkaLog.py
    <IkaInput_CVCapture.IkaInput_CVCapture object at 0x101eb1ba8>: initalizing pre-recorded video file /Users/hasegaw/work/splatoon/hoko_win.mp4
    [event] 00:00 onLobbyMatched
    ゲームスタート。マップ モズク農園 ルール ガチホコバトル
    [event] 00:08 onGameStart
    <IkaOutput_Ikadenwa.IkaOutput_Ikadenwa object at 0x1096d71d0>: Ikadenwa 操作 (全員ミュート)
    /usr/local/lib/python3.4/site-packages/numpy/core/_methods.py:59: RuntimeWarning: Mean of empty slice.
    warnings.warn("Mean of empty slice.", RuntimeWarning)
    /usr/local/lib/python3.4/site-packages/numpy/core/_methods.py:71: RuntimeWarning: invalid value encountered in double_scalars
    ret = ret.dtype.type(ret / rcount)
    [event] 00:16 onGameTeamColor
    <IkaOutput_Ikadenwa.IkaOutput_Ikadenwa object at 0x1096d71d0>: 自分 カラー 84 マッチ範囲 [(74, 94)]
    <IkaOutput_Ikadenwa.IkaOutput_Ikadenwa object at 0x1096d71d0>: Ikadenwa 操作(上記範囲にいる他のプレイヤーをミュート解除
    <IkaOutput_Ikadenwa.IkaOutput_Ikadenwa object at 0x1096d71d0>: 相手 カラー 37 マッチ範囲 [(27, 47)]
    [event] 00:16 onGameGoSign
    [event] 00:17 onGameDead
    [event] 00:43 onGameDead
    [event] 00:55 onGameDead
    [event] 01:02 onGameDead
    [event] 01:16 onGameDead
    [event] 01:38 onGameDead
    [event] 02:00 onGameDead
    [event] 02:30 onGameDead
    [event] 02:35 onGameFinish
    <IkaOutput_Ikadenwa.IkaOutput_Ikadenwa object at 0x1096d71d0>: Ikadenwa 操作 (全員ミュート解除)
    ゲーム終了。 ステージ:モズク農園 ルール:ガチホコバトル 勝ち 7K/5D 使用ブキ:3Kスコープ チーム内順位: 1
    <IkaOutput_CSV.IkaOutput_CSV object at 0x1096dcbe0> (enabled = True)
    <IkaOutput_JSON.IkaOutput_JSON object at 0x1096dce48> (enabled = True)
    [event] 02:50 onGameIndividualResult
    ^C^C^CIkaLog: got signal 2
    
    Signed-of-by: Takeshi HASEGWA <hasegaw@gmail.com>

--- IkaOutput_Ikadenwa.py
+++ IkaOutput_Ikadenwa.py
@@ -22,34 +22,46 @@ from IkaUtils import *
 from datetime import datetime
 import time
 
-## IkaLog Output Plugin: Show message on Console
+# IkaLog Output Plugin: Show message on Console
 #
+
+
 class IkaOutput_Ikadenwa:
 
-	def onLobbyMatched(self, context):
-		# ToDo: もし使えるのなら全プレイヤーの情報(ランク、名前のハッシュ的なもの)をイカデンワに送信
-		# イカデンワ側でのユーザマッチングに利用してもらう 
+    def onLobbyMatched(self, context):
+        # ToDo: もし使えるのなら全プレイヤーの情報(ランク、名前のハッシュ的なもの)をイカデンワに送信
+        # イカデンワ側でのユーザマッチングに利用してもらう
+        pass
 
-	def onGameStart(self, context):
-		# ゲーム開始でステージ名、ルール名が表示されたタイミング
+    def onGameStart(self, context):
+        # ゲーム開始でステージ名、ルール名が表示されたタイミング
+        IkaUtils.dprint('%s: Ikadenwa 操作 (全員ミュート)' % self)
 
-		# ToDo: いったん全員ミュート
+    def onGameTeamColor(self, context):
+        # チームカラーが判明したらイカデンワに報告する
 
-        def onTeamColor(self, context):
-                # チームカラーが判明したらイカデンワに報告する
-		my_color = context['game']['color'][0]
+        for n in range(len(context['game']['team_color_hsv'])):
+            team = {0: '自分', 1: '相手'}[n]
+            hsv = context['game']['team_color_hsv'][n]
 
-		# ToDo: イカデンワに色を通知(してミュートを解除してほしい)
-		#       色情報は整数値(HSV) の Hue 、整数値で送信する
-		#       ±20度ぐらいでおなじ色を持っているプレイヤーとマッチングして
-		#       自動的にミュートを解除してもらえると OK
+            # ±10度でマッチングすれば多分OK
+            hue = hsv[0]
+            range1 = (max(0, hue - 10), min(180, hue + 10))
+            range2 = (min(180, hue - 10 + 180), min(180, hue + 10 + 180))
 
-	def onGameFinished(self, context):
-		# まだないコールバック
-		# ゲーム終了が検出できたらできるだけ早くこのコールバックが呼ばれるようにする
+            l = [range1]
+            if range2[0] < 180:
+                l.append(range2)
 
-		# ToDo: イカデンワの Mute 解除
+            IkaUtils.dprint('%s: %s カラー %d マッチ範囲 %s' % (self, team, hue, l))
+            if n == 0:
+                IkaUtils.dprint('%s: Ikadenwa 操作(上記範囲にいる他のプレイヤーをミュート解除' % self)
 
-	def onGameIndividualResult(self, context):
-		# いまでもあるコールバック
-		# このタイミングでミュートだとちょっと遅い
+        # ToDo: イカデンワに色を通知(してミュートを解除してほしい)
+        #       色情報は整数値(HSV) の Hue 、整数値で送信する
+        #       ±10度ぐらいでおなじ色を持っているプレイヤーとマッチングして
+        #       自動的にミュートを解除してもらえると OK
+
+    def onGameFinish(self, context):
+        # マッチが終了したと同時に全員ミュート解除
+        IkaUtils.dprint('%s: Ikadenwa 操作 (全員ミュート解除)' % self)
